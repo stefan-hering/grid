343ad9457fec7cc1a3f21fe04041b71a6730e287
stefan-hering <stefanvhering@gmail.com>
2017-07-08 21:22:38 +0200

    Removed output from test

--- test/testlexer.ts
+++ test/testlexer.ts
@@ -5,6 +5,6 @@ import { suite, test, slow, timeout } from "mocha-typescript";
 suite("Lexer", () => {
     test("Should parse grid cells into tokens", () => {
         lexGridCell("test");
-        console.log(lexGridCell("i\ni = 3 left\n i = 5 right"));
+        lexGridCell("i\ni = 3 left\n i = 5 right");
     });
 });
