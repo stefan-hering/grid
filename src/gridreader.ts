b4e7198207885f7de9464e0d71e3beb20400cde1
stefan-hering <stefanvhering@gmail.com>
2017-08-07 00:06:38 +0200

    Some work on reader

--- src/gridreader.ts
+++ src/gridreader.ts
@@ -1,6 +1,29 @@
+import {Grid} from "grid";
 
-function parseGrid(grid : String) {
-    // read width
-    // read height
-    // read blocks
+export function readGrid(grid : String) : String[][] {
+    let atIndex : number = grid.indexOf("@");
+    // The height is simply the number of linebreaks before the first @
+    let height : number = grid.split("@")[0].match("\n").length;
+    // The width is the index of the first add minus the length of all the lines above it.
+    let width : number = atIndex - (grid.indexOf("\n") + 1) * height;
+    // The number of cells in one row of the grid
+    let horizontalCells : number = (grid.indexOf("\n") + 1) / (width + 1);
+
+    let parsedGrid : String[][] = [];
+    let lines : String[] = grid.split("\n");
+    let row = 0;
+
+    for(let i = 0; i < lines.length; i++){
+        if(i % (height + 1) == height){
+            // Each (height + 1)th row is a placeholder, so we just increase the row counter
+            row++;
+        } else {
+            for(let j = 0; j < horizontalCells; j++){
+                parsedGrid[row][j] = lines[i].slice(j * (width + 1),
+                    j * (width + 1) + width) + "\n";
+            }
+        }
+    }
+
+    return parsedGrid;
 }
---
+++ test/testgridreader.ts
@@ -0,0 +1,8 @@
+import {readGrid} from "../src/gridreader";
+import { suite, test, slow, timeout } from "mocha-typescript";
+
+suite("Lexer", () => {
+    test("Should parse grid cells into tokens", () => {
+
+    });
+});
