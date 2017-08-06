8b5c19744106d841ac5c38e19b69cb58bb9a1e94
stefan-hering <stefanvhering@gmail.com>
2017-08-07 00:25:00 +0200

    Made test worka nd fixed some bugs in gridreader

--- src/gridreader.ts
+++ src/gridreader.ts
@@ -1,22 +1,24 @@
 import {Grid} from "grid";
 
 export function readGrid(grid : String) : String[][] {
-    let atIndex : number = grid.indexOf("@");
+    let tillFirstAt : String = grid.split("@")[0]
     // The height is simply the number of linebreaks before the first @
-    let height : number = grid.split("@")[0].match("\n").length;
-    // The width is the index of the first add minus the length of all the lines above it.
-    let width : number = atIndex - (grid.indexOf("\n") + 1) * height;
+    let height : number = tillFirstAt.match(/\n/g).length;
+    // The width is the amount of characters between the @ and the previous linebreak
+    let width : number = tillFirstAt.split("\n")[height].length;
     // The number of cells in one row of the grid
-    let horizontalCells : number = (grid.indexOf("\n") + 1) / (width + 1);
+    let horizontalCells : number = Math.floor((grid.indexOf("\n") + 1) / (width + 1));
 
     let parsedGrid : String[][] = [];
     let lines : String[] = grid.split("\n");
     let row = 0;
+    parsedGrid[0] = [];
 
     for(let i = 0; i < lines.length; i++){
         if(i % (height + 1) == height){
             // Each (height + 1)th row is a placeholder, so we just increase the row counter
             row++;
+            parsedGrid[row] = [];
         } else {
             for(let j = 0; j < horizontalCells; j++){
                 parsedGrid[row][j] = lines[i].slice(j * (width + 1),
--- test/testgridreader.ts
+++ test/testgridreader.ts
@@ -1,8 +1,13 @@
 import {readGrid} from "../src/gridreader";
 import { suite, test, slow, timeout } from "mocha-typescript";
+import { expect } from 'chai';
+let fs = require("fs");
 
-suite("Lexer", () => {
-    test("Should parse grid cells into tokens", () => {
-
+suite("Reader", () => {
+    test("Should read the individual cells of a grid", () => {
+        let testgrid : String = fs.readFileSync("test/grids/test.grid", "utf8");
+        let grid : String[][] = readGrid(testgrid);
+        expect(grid.length).to.equal(3);
+        expect(grid[0].length).to.equal(3);
     });
 });
